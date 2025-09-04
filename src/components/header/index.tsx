e5272519c919dd45878c5b0493349189cd00c6e7
Jonatas Falkaniere <jonatasfalkaniere@gmail.com>
2025-09-03 23:26:55 -0300

    chore: split header

---
+++ src/components/header/index.tsx
@@ -0,0 +1,20 @@
+import { Text, View } from 'react-native';
+import Emoji from 'react-native-emoji';
+
+import { styles } from './styles';
+
+jest.mock('react-native-emoji', () => 'Emoji');
+
+function Header() {
+  return (
+    <View style={styles.header}>
+      <Text style={styles.title}>My Fridge</Text>
+      <View style={styles.headerIcons}>
+        <Emoji name='add' />
+        <Emoji name='options' />
+      </View>
+    </View>
+  );
+}
+
+export default Header;
---
+++ src/components/header/styles.ts
@@ -0,0 +1,8 @@
+import { StyleSheet } from "react-native";
+
+
+export const styles = StyleSheet.create({
+  header: { flexDirection: "row", justifyContent: "space-between", alignItems: "center" },
+  title: { fontSize: 24, fontWeight: "bold" },
+  headerIcons: { flexDirection: "row" },
+})
