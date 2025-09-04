75a0ec74015cd88ca765ada17453ac629c20a162
Jonatas Falkaniere <jonatasfalkaniere@gmail.com>
2025-09-03 22:59:46 -0300

    chore: added eslint and prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,11 +1,29 @@
 module.exports = {
   root: true,
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
   extends: [
     "eslint:recommended",
-    "plugin:prettier/recommended"
+    "plugin:react/recommended",
+    "plugin:react-hooks/recommended",
+    "plugin:react-native/all",
+    "prettier", // turn off ESLint rules that conflict with Prettier
   ],
-  plugins: ["prettier"],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  plugins: ["react", "react-hooks", "react-native", "prettier"],
   rules: {
-    "prettier/prettier": ["error", { "singleQuote": true }]
-  }
+    "prettier/prettier": "error", // show prettier errors in ESLint
+    "react/react-in-jsx-scope": "off", // not needed in React Native
+  },
+  settings: {
+    react: {
+      version: "detect",
+    },
+  },
 };
