524f47b1f9dbb5dc6a6d5a9a50101059a27d5503
Jonatas Falkaniere <jonatasfalkaniere@gmail.com>
2026-01-10 00:47:02 -0300

    refact: add alias;

--- babel.config.js
+++ babel.config.js
@@ -1,4 +1,13 @@
 module.exports = {
-  presets: ['module:@react-native/babel-preset'],
-  plugins: [['module-resolver', { alias: { '@': './src' } }]],
+  presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['./'],
+        alias: { '@': './src' },
+        extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
+      },
+    ],
+  ],
 };
