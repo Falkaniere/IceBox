8a722aa16febd4a000a4a6d106644490101cd1d9
Jonatas Falkaniere <jonatasfalkaniere@gmail.com>
2026-02-26 00:25:26 -0300

    feat: vibrar ao deletar

--- src/features/fridge/components/FridgeItem/index.tsx
+++ src/features/fridge/components/FridgeItem/index.tsx
@@ -2,18 +2,24 @@ import React from 'react';
 import { Text, View, Pressable, Alert } from 'react-native';
 import { Trash2 } from 'lucide-react-native';
 
-import { formatExpiryLabel, ExpiryStatus } from '@/app/utils/expiry';
+import { formatExpiryLabel } from '@/app/utils/expiry';
 import { styles } from './styles';
 import { categoryIconMap } from '@/features/fridge/utils/categoryIcon';
 import { FridgeItem as FridgeItemType } from '@/features/fridge/model/fridgeItem';
 import { useFridge } from '@/features/fridge/providers/FridgeProvider';
 import { useAppTranslation } from '@/app/i18n/useAppTranslation';
+import ReactNativeHapticFeedback from 'react-native-haptic-feedback';
 
 // function getStatusLabel(status: ExpiryStatus | undefined) {
 //   if (status === 'expired') return 'Expired';
 //   if (status === 'soon') return 'Expiring soon';
 //   return 'Fresh';
 // }
+
+const hapticOptions = {
+  enableVibrateFallback: true,
+  ignoreAndroidSystemSettings: false,
+};
 
 export default function FridgeItem({
   item,
@@ -26,6 +32,8 @@ export default function FridgeItem({
   const { t } = useAppTranslation('common');
 
   function handleDelete() {
+    ReactNativeHapticFeedback.trigger('impactLight', hapticOptions);
+
     Alert.alert(t('confirm_delete_title'), t('confirm_delete_message'), [
       {
         text: t('cancel'),
@@ -34,7 +42,11 @@ export default function FridgeItem({
       {
         text: t('delete'),
         style: 'destructive',
-        onPress: () => removeItem(item.id),
+        onPress: () => {
+          ReactNativeHapticFeedback.trigger('impactMedium', hapticOptions);
+
+          removeItem(item.id);
+        },
       },
     ]);
   }
